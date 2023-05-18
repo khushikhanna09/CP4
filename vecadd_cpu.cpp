a49413aa0046c06cccec783a24e30ede85562856
khushikhanna09 <98506089+khushikhanna09@users.noreply.github.com>
2023-05-18 13:33:41 -0700

    Update vecadd_cpu.cpp

--- vecadd_cpu.cpp
+++ vecadd_cpu.cpp
@@ -27,7 +27,7 @@ int main(void)
 
   std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> elapsed = end_time - start_time;
-  std::cout << " Elapsed time is : " << elapsed.count() << " " << std::endl;
+  std::cout << "Elapsed time is : " << elapsed.count() << " " << std::endl;
 
   float maxError = 0.0f;
   for (int i = 0; i < N; i++)
